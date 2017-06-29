ca75b82a03dd5bef8d22d6c26ff496661df40393
Miguel Fernández Villegas <miguelfervi@gmail.com>
2017-06-29 09:39:51 +0200

    Cambios en icono y cteacion de actividades

--- app/src/main/java/com/tfm/miguel/planetaria/Activities/Ejercicio1Activity.java
+++ app/src/main/java/com/tfm/miguel/planetaria/Activities/Ejercicio1Activity.java
@@ -2,6 +2,10 @@ package com.tfm.miguel.planetaria.Activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.tfm.miguel.planetaria.R;
 
@@ -11,5 +15,30 @@ public class Ejercicio1Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ejercicio1);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        myToolbar.setTitle("DESSI");
+        setSupportActionBar(myToolbar);
+
+
+        Window window = this.getWindow();
+
+        // clear FLAG_TRANSLUCENT_STATUS flag:
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+
+        // add FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS flag to the window
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+
+        // finally change the color
+        window.setStatusBarColor(getResources().getColor(R.color.colorPrimaryDark));
+
+        myToolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
     }
 }
